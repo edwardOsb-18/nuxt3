12b663be1e89529401a75b22e9c2d3cba44042ad
edward <you@gmail.com>
2023-11-07 20:49:28 -0500

    primer

---
+++ pages/osb/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>hola {{ titulo }}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore inventore qui ullam at est. Repellat provident hic nam unde.</p>
+    
+    </div>
+  </template>
+  
+  <script setup>
+const route=useRoute()
+
+const titulo=route.params.id
+  
+
+  </script>
